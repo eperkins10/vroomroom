cca4d756c3452fd0d009150258c53883c7e3d6fa
Tanner Perry <tc-boy@hotmail.com>
2021-04-13 00:23:36 -0600

    tp: reformatted the car class

--- Vroom Vroom/Vroom Vroom/Car.swift
+++ Vroom Vroom/Vroom Vroom/Car.swift
@@ -24,12 +24,16 @@ public enum DriveType {
 
 public enum Manufacturer : String {
     case audi = "Audi"
+    case bmw = "BMW"
+    case chevrolet = "Chevrolet"
     case dodge = "Dodge"
     case ferarri = "Ferarri"
+    case ford = "Ford"
     case koenigsegg = "Koenigsegg"
     case lamborghini = "Lamborghini"
     case lexus = "Lexus"
     case mclaren = "Mclaren"
+    case mercedezbenz = "Mercedez Benz"
     case nissan = "Nissan"
     case porshe = "Porshe"
 }
@@ -43,7 +47,7 @@ public class Car {
     public var urlName : String
     
     public init (carModel:String, carOrigin:Origin, driveType:DriveType, cylinderCount:Int, manufacturer:Manufacturer,
-        urlName:String) {
+                 urlName:String) {
         self.carModel = carModel
         self.carOrigin = carOrigin
         self.driveType = driveType
@@ -59,16 +63,26 @@ public class Car {
     }
     
     static var CarList = [Car(carModel: "RS7", carOrigin: .german, driveType: .fourWheelDrive, cylinderCount: 8, manufacturer: .audi, urlName: "audi_rs7"),
-                   Car(carModel: "S4", carOrigin: .german, driveType: .fourWheelDrive, cylinderCount: 6, manufacturer: .audi, urlName: "audi_s4"),
-                   Car(carModel: "Hellcat", carOrigin: .american, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .dodge, urlName: "dodge_hellcat"),
-                   Car(carModel: "458", carOrigin: .italian, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .ferarri, urlName: "ferarri_458"),
-                   Car(carModel: "Agera RS", carOrigin: .swiss, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .koenigsegg, urlName: "koenigsegg_agera_r"),
-//                   Car(carModel: "Aventador", carOrigin: .italian, driveType: .fourWheelDrive, cylinderCount: 12, manufacturer: .Lamborghini, urlName: "Lamborghini Aventador"),
-                   Car(carModel: "LFA", carOrigin: .japanese, driveType: .twoWheelDrive, cylinderCount: 10, manufacturer: .lexus, urlName: "lexus_lfa"),
-                   Car(carModel: "P1", carOrigin: .uk, driveType: .fourWheelDrive, cylinderCount: 8, manufacturer: .mclaren, urlName: "mclaren_p1"),
-                   Car(carModel: "GTR", carOrigin: .japanese, driveType: .fourWheelDrive, cylinderCount: 6, manufacturer: .nissan, urlName: "nissan_r35_gtr"),
-                   Car(carModel: "Carrera GT", carOrigin: .german, driveType: .twoWheelDrive, cylinderCount: 10, manufacturer: .porshe, urlName: "porshe_garrera_gt")
-        ]
+                          Car(carModel: "S4", carOrigin: .german, driveType: .fourWheelDrive, cylinderCount: 6, manufacturer: .audi, urlName: "audi_s4"),
+                          Car(carModel: "Hellcat", carOrigin: .american, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .dodge, urlName: "dodge_hellcat"),
+                          Car(carModel: "458", carOrigin: .italian, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .ferarri, urlName: "ferarri_458"),
+                          Car(carModel: "Agera RS", carOrigin: .swiss, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .koenigsegg, urlName: "koenigsegg_agera_r"),
+                          //                   Car(carModel: "Aventador", carOrigin: .italian, driveType: .fourWheelDrive, cylinderCount: 12, manufacturer: .Lamborghini, urlName: "Lamborghini Aventador"),
+                          Car(carModel: "LFA", carOrigin: .japanese, driveType: .twoWheelDrive, cylinderCount: 10, manufacturer: .lexus, urlName: "lexus_lfa"),
+                          Car(carModel: "P1", carOrigin: .uk, driveType: .fourWheelDrive, cylinderCount: 8, manufacturer: .mclaren, urlName: "mclaren_p1"),
+                          Car(carModel: "GTR", carOrigin: .japanese, driveType: .fourWheelDrive, cylinderCount: 6, manufacturer: .nissan, urlName: "nissan_r35_gtr"),
+                          Car(carModel: "Carrera GT", carOrigin: .german, driveType: .twoWheelDrive, cylinderCount: 10, manufacturer: .porshe, urlName: "porshe_garrera_gt"),
+                          
+                          Car(carModel: "Silverado LS", carOrigin: .american, driveType: .fourWheelDrive, cylinderCount: 8, manufacturer: .chevrolet, urlName: "chevrolet_ls_silverado_48l"),
+                          Car(carModel: "Silverado LBZ", carOrigin: .american, driveType: .fourWheelDrive, cylinderCount: 8, manufacturer: .chevrolet, urlName: "chevrolet_silverado_lbz_duramax"),
+                          Car(carModel: "F90", carOrigin: .german, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .bmw, urlName: "f90_bmw_m5"),
+                          Car(carModel: "F150 Ecoboost", carOrigin: .american, driveType: .fourWheelDrive, cylinderCount: 6, manufacturer: .ford, urlName: "ford_f150_ecoboost"),
+                          Car(carModel: "F250 Powerstroke", carOrigin: .american, driveType: .fourWheelDrive, cylinderCount: 8, manufacturer: .ford, urlName: "ford_f250_powerstroke_60"),
+                          Car(carModel: "Mustang GT", carOrigin: .american, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .ford, urlName: "ford_mustang_gt"),
+                          Car(carModel: "E63 AMG", carOrigin: .german, driveType: .twoWheelDrive, cylinderCount: 8, manufacturer: .mercedezbenz, urlName: "mercedez_benz_e63_amg"),
+                          Car(carModel: "GT2RS", carOrigin: .german, driveType: .twoWheelDrive, cylinderCount: 6, manufacturer: .porshe, urlName: "porsche_911_gt2rs"),
+                          Car(carModel: "GT3RS", carOrigin: .german, driveType: .twoWheelDrive, cylinderCount: 6, manufacturer: .porshe, urlName: "porsche_911_gt3rs")
+    ]
     
     
 }
